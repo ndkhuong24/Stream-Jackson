df54464c1d20374c1a490ec1dae1c5d80ea0540e
Nguyễn Duy Khương <kn134646@gmail.com>
2023-07-17 22:46:53 +0700

    up

--- src/main/java/com/example/demo/controller/PeopleController.java
+++ src/main/java/com/example/demo/controller/PeopleController.java
@@ -1,9 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.People;
 import com.example.demo.repository.IPeopleRepository;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/people")
@@ -39,7 +33,7 @@ public class PeopleController {
         }
         int startIndex = (page - 1) * pageSize;
         int endIndex = Math.min(startIndex + pageSize, totalElements);
-        List<Map<String, Object>> students =people.subList(startIndex, endIndex);
+        List<Map<String, Object>> students = people.subList(startIndex, endIndex);
         model.addAttribute("students", students);
         model.addAttribute("totalPages", totalPages);
         model.addAttribute("currentPage", page);
